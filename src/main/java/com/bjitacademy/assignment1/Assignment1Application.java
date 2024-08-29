62955e6ee08e494bda1db5be8222b69c8efdc863
Ashraf BJIT <ashraful.huque@bjitgroup.com>
2024-08-29 10:27:45 +0600

    added logger

--- src/main/java/com/bjitacademy/assignment1/Assignment1Application.java
+++ src/main/java/com/bjitacademy/assignment1/Assignment1Application.java
@@ -1,13 +1,18 @@
 package com.bjitacademy.assignment1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Assignment1Application {
 
+	public static final Logger logger = LoggerFactory.getLogger(Assignment1Application.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(Assignment1Application.class, args);
+		logger.info("its a continous integration job");
 	}
 
 	public void test() {
